a79e4ddd57a66298f693e6fe2d1ff300f8d766e4
Evgenii <evgeny1888@bk.ru>
2022-02-06 13:33:41 +0300

    first commit

---
+++ training/js/script.js
@@ -0,0 +1,36 @@
+"use strict";
+/* const obj = {
+    age: 15,
+    sex: "male",
+    isMarried: true
+};
+
+let arr = ['zakhar', 'sasha', 'evgenii'];
+console.log(arr[2]);
+
+const result = confirm("Are u pidor?");
+console.log(result);
+*/
+// const answer = prompt("How old are u?", "18");
+// console.log(typeof(answer));
+
+// const answers = [];
+// answers[0]=prompt("What's ur name?", "");
+// answers[1]=prompt("What's ur surname?", "");
+// answers[2]=prompt("What's ur age?", "");
+// document.write("what's the fuck?");
+
+// const   category='toys';
+// console.log(`http://someurl.com/${category}`);
+
+// const user="Evgenii";
+// alert(`Hello, ${user}`);
+
+// console.log(5+ +'9');
+
+let incr = 10,
+    decr = 10;
+incr++;
+decr--;
+console.log(incr);
+console.log(decr);
